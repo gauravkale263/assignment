11ee6ac17d5ded9b711f39f0230879517464f267
Gaurav Kale <gauravkale2509@gmail.com>
2023-10-02 11:08:13 +0530

    two-files-sorting-romanToInt

---
+++ sorting.js
@@ -0,0 +1,19 @@
+function sortingArray(arr)
+{
+    for(let i=0;i<arr.length;i++)
+    {
+        for(let j=0;j<arr.length;j++)
+        {
+            if(arr[i]>arr[j])
+            {
+                let temp=arr[i];
+                arr[i]=arr[j]
+                arr[j]=temp
+            }
+        }
+    }
+}
+let arr=[5,3,8,6,0,9]
+console.log("Array before sorting:",arr);
+sortingArray(arr)
+console.log("Array after sorting:",arr)
